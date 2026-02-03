1a6ce180dd7da4def733bc8cd008dca3bc46f14a
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-02-03 15:35:03 +0100

    commit

--- Q4_2/Q4_2.cpp
+++ Q4_2/Q4_2.cpp
@@ -16,41 +16,42 @@ tableau. Pour cela, on utilisera un "tri à bulle".*/
 #include<string.h>
 
 float Lire(float* a) {
+	int sauve = 0;
 
-	printf("Entrez les valeurs (pas plus de 20) : \n");
+	printf("Entrez les valeurs (pas plus de 20 et saisire -1 pour arreter la saisie) : \n");
 
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 20; i++){
+		
 		scanf_s("%f", &a[i]);
-		if(a[i] == -1){
-			//a[i] = NAN;
-			//while(i < 20){
-			
-			//}
+		if (a[i] == -1) {
+			while (i < 20) {
+				a[i] = 0;
+				i++;
+			}
 		}
+		sauve++;
 	}
 
-	printf("\n");
-
-	return 0;
+	return sauve;
 }
 
 float affiche(float* b) {
 	printf("Affichage des resultats : \n");
-
-	for (int i = 0; i < 20; i++) {
-		printf_s("%f\n",b[i]);
-	}
 	
-	printf_s("\n");
+	for (int j = 0; j < 20; j++) {
+		printf_s("%f\n", b[j]);
+	}
 
 	return 0;
 }
 
-float indice_min(float* tab, int i, float* j) {
-	i = 0;
+float indice_min(float* tab, int max) {
+	int i = 0;
+	int j[1];
+
 	j[0] = tab[0];
 	
-	while (i < 20) {
+	while (i < max - 1) {
 		if (j[0] > tab [i]){
 			j[0] = tab[i];
 		}
@@ -60,19 +61,31 @@ float indice_min(float* tab, int i, float* j) {
 	return j[0];
 }
 
-float trier(float a) {
+float trier(float* tab, float trie) {
+	for (int j = 0; j < 20; j++) {
+		for (int i = 0; i < 20 - 1; i++) {
+			if (tab[i] > tab[i + 1]){
+				trie = tab[i + 1];
+				tab[i + 1] = tab[i];
+				tab[i] = trie;
+			}
+		}
+	}
 
 	return 0;
 }
 
 int main() {
 	float tab[20];
-	int a = 0;
 	float b[2];
 
-	Lire(tab);
+	int max = Lire(tab);
+
+	float trie = indice_min(tab, max);
+
+	trier(tab, trie);
+
 	affiche(tab);
-	float trie = indice_min(tab, a, b);
-	printf_s("La valeur la plus base est : %f\n",b[0]);
-	trier(trie);
+
+	return 0;
 }
